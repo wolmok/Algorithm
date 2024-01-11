3100d1267bbd60056438f1b1595b6e7cc1defde6
yhkim1 <93534188+yihyun-kim1@users.noreply.github.com>
2024-01-11 10:52:20 +0900

    [failed] Boj_16496.py

---
+++ KimYihyun /백준/Boj_16496.py
@@ -0,0 +1,12 @@
+# 큰 수 만들기
+N = int(input())
+numbers = list(map(int, input().split()))
+
+for i in range(N):
+    if numbers[i] == '0': 
+        #0을 포함하는 리스트일 경우
+        
+        
+    else: 
+        #0을 포함하지 않는 리스트일 경우
+        for j in range():
