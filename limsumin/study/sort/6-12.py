d3c16c07be2264732fb0382d32b9f37b89a213f6
soomin9106 <soomin9106@naver.com>
2024-01-18 11:09:13 +0900

    [fix] 6-12.py

--- limsumin/study/sort/6-12.py
+++ limsumin/study/sort/6-12.py
@@ -12,6 +12,11 @@ B = list(map(int, input().split()))
 A = sorted(A)
 B = sorted(B, reverse= True)
 
-result = sum(A[K:]) + sum(B[:K])
+# 이렇게 하면 A 에 있는 원소들이 B 보다 클 수도 있는 경우를 고려 못함
+# result = sum(A[K:]) + sum(B[:K])
 
-print(result)
+for i in range(K):
+    if A[i] < B[i]:
+        A[i] = B[i]
+
+print(sum(A))
