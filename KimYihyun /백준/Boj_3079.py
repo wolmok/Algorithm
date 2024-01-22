12b3b55a5acc60289560a6728a4cb62ae5ccfbf3
yhkim1 <93534188+yihyun-kim1@users.noreply.github.com>
2024-01-23 00:08:55 +0900

    [failed] Boj_3079.py
    로직은 맞았으나 막판에 total 밖으로 빼서 초기화해서 틀린 사람 .. 저에요 .. ^^TTT

--- KimYihyun /백준/Boj_3079.py
+++ KimYihyun /백준/Boj_3079.py
@@ -5,17 +5,29 @@
 # M : 총 인원
 N, M = map(int, input().split())
 # Tk : 입국 심사에 소요되는 시간
-Tk = list(map(int, input().split()))
+Tk = []
+for _ in range(N):
+    Tk.append(int(input()))
 # 오름차순으로 정렬, 따라서 0번째 인덱스가 소요되는 최소 시간
-Tk.sort()
+Tk = sorted(Tk)
 
-start, end = 0, M - 1
-max_time = Tk[-1] * M
+start = Tk[0]
+end = Tk[-1] * M
+res = float('inf')
 
 while start <= end:
     global total
+    total = 0
     mid = (start + end) // 2
-    for i in Tk:
-        # total += i * M  ====> total을 어떻게 계산해야 할 지
+    for t in Tk:
+        # mid라는 시간 동안 심사 가능한 인원 수 파악
+        total += (mid // t)
 
-print(total)
+    # 만약 mid 시간 동안 M명을 심사하고 남는다면 끝점을 이동
+    if (total >= M):
+        end = mid - 1
+        res = min(res, mid)
+    # 아니라면 시작점을 이동
+    else:
+        start = mid + 1
+print(res)
