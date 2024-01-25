40b19b0e18706d8c8ea19ac874aae8f98f0f291f
yhkim1 <93534188+yihyun-kim1@users.noreply.github.com>
2024-01-25 11:49:02 +0900

    [solved] Boj_4929.py

--- KimYihyun /백준/Boj_4929.py
+++ KimYihyun /백준/Boj_4929.py
@@ -1,50 +1,44 @@
 # 수열 걷기
 # https://www.acmicpc.net/problem/4929
 
-sum_first = 0  # 첫 번째 수열에서 시작한 합
-sum_second = 0  # 두 번째 수열에서 시작한 합
-res = 0  # 최종 결과
-
-
-def sequence(array1, array2):
-    global len_ary
-    if len(array1) > len(array2):
-        len_ary = len(array1)
-    elif len(array1) < len(array2):
-        len_ary = len(array2)
-    else:
-        len_ary = len(array1)  # 어떤 리스트의 길이만큼 반복해도 상관 없음
-    for i in range(len_ary):
-        # 다를 경우 그냥 더함
-        if array1[i] != array2[i]:
-            sum_first += array1[i]
-            sum_second += array2[i]
-        else:  # 같아지면
-            mid = (array1[i] + array2[i]) // 2
-            if array1[i + 1] > mid:
-                sum_first += array2[i]
-                sum_second += array2[i]
-            elif array2[i + 1] < mid:
-                sum_first += array1[i]
-                sum_second += array1[i]
-    if sum_first > sum_second:
-        res = sum_first
-    else:
-        res = sum_second  # 만약 같더라도 상관 없음
-    print("res:", res)
-
-
 while True:
     array1 = list(map(int, input().split()))
     if array1[0] == 0:
         break
     else:
         array2 = list(map(int, input().split()))
-        # print(array1, array2)
-        # 1번째 원소부터 저장한 리스트
         first_array = array1[1:]
         second_array = array2[1:]
-        # print(first_array, second_array)
+        sum_first, sum_second = 0, 0
+        idx_first, idx_second = 0, 0
+        result = 0
 
+        # 첫 번째 원소 - 리스트의 길이
+        while idx_first < array1[0] and idx_second < array2[0]:
+            if first_array[idx_first] == second_array[idx_second]:
+                result += max(sum_first, sum_second)  # 누적합을 먼저 더하고
+                result += first_array[idx_first]  # 둘 중 아무 값이나 더해줌 (같으므로)
+                sum_first, sum_second = 0, 0
+                idx_first += 1
+                idx_second += 1
+            elif idx_first < array1[0] and first_array[idx_first] < second_array[idx_second]:
+                # 범위 안에 있고, array1의 원소가 더 작을 경우
+                sum_first += first_array[idx_first]
+                idx_first += 1
+            elif idx_second < array2[0] and second_array[idx_second] < first_array[idx_first]:
+                # 범위 안에 있고, array2의 원소가 더 작을 경우
+                sum_second += second_array[idx_second]
+                idx_second += 1
 
-print(sequence(first_array, second_array))
+        # 남은 부분 처리
+        while idx_first < array1[0]:
+            sum_first += first_array[idx_first]
+            idx_first += 1
+
+        while idx_second < array2[0]:
+            sum_second += second_array[idx_second]
+            idx_second += 1
+
+        result += max(sum_first, sum_second)
+
+        print(result)
