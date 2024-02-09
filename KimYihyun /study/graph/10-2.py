8a34e10fca016f4056ee64b471db435e1d20b40b
yhkim1 <93534188+yihyun-kim1@users.noreply.github.com>
2024-02-10 00:25:06 +0900

    Create 10-2.py

---
+++ KimYihyun /study/graph/10-2.py
@@ -0,0 +1,6 @@
+def find_parent(parent, x):
+  if parent[x] != x:
+    parent[x] = find_parent(parent, parent[x])
+  return parent[x]
+
+# 경로를 따라 올라가며 부모를 찾는 것이 아니라, 노드 x에서 루트까지 경로상 모든 노드의 부모를 직접 루트로 설정
