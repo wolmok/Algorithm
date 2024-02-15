ed41006b2ab1885caa0a6db21cfdbd607d0d5d9f
yhkim1 <93534188+yihyun-kim1@users.noreply.github.com>
2024-02-15 09:35:18 +0900

    Update Boj_9079.py
    비트 마스킹에 대한 학습 X로 방향을 다르게 잡고 있어서, 참고 코드를 사용해 학습했습니다 !!

--- KimYihyun /백준/Boj_9079.py
+++ KimYihyun /백준/Boj_9079.py
@@ -1,30 +1,49 @@
 # 동전 게임
 # https://www.acmicpc.net/problem/9079
 
+def game(coins):
+    global result
+    for bit_mask in range(2**8):
+        copy_coins = [row[:] for row in coins]
+        change_bit = str(bin(bit_mask))[2:].count('1')
+        if result < change_bit:
+            continue
+        for row in range(3):
+            if bit_mask & (1 << row):
+                for col in range(3):
+                    copy_coins[row][col] = (copy_coins[row][col]+1) % 2
+
+        for col in range(3):
+            if bit_mask & (1 << (col+3)):
+                for row in range(3):
+                    copy_coins[row][col] = (copy_coins[row][col]+1) % 2
+
+        if bit_mask & (1 << 6):
+            for row in range(3):
+                copy_coins[row][row] = (copy_coins[row][row]+1) % 2
+
+        if bit_mask & (1 << 7):
+            for row in range(3):
+                copy_coins[row][2-row] = (copy_coins[row][2-row]+1) % 2
+
+        sum_temp = sum(list(map(sum, copy_coins)))
+        if sum_temp == 9 or sum_temp == 0:
+            result = change_bit
+
+
 T = int(input())
-
-
-def flip(coins):
-    # 동전의 상태 -> 문자열로 저장
-    coin_list = set([''.join(row) for row in coins])  # 여기서 구글링함
-    count = 0
-
-    # all 동일한 상태라면
-    if len(coin_list) == 1:
-        return 0
-
-    # for i in range(3):
-    # 행
-    #     if coins[0][i] == coins[1][i] == coins[2][i]:
-    # 열
-    #     if coins[i][0] == coins[i][1] == coins[i][2]:
-
-    if count >= 0:
-        return count
-    else:
-        return -1
-
-
 for _ in range(T):
-    coins = [input().split() for _ in range(3)]
-    print(flip(coins))
+    coins = []
+    result = float('inf')
+    for _ in range(3):
+        a = list(input().split())
+        for i in range(3):
+            if a[i] == 'T':
+                a[i] = 1
+            else:
+                a[i] = 0
+        coins.append(a)
+    cnt = 0
+
+    game(coins)
+    print(-1 if result == float('inf') else result)
