468f4e20c5115a9abd0f3a747935feb5a63dae98
yhnny <93534188+yihyun-kim1@users.noreply.github.com>
2024-02-28 14:04:50 +0900

    Update 28.py

--- KimYihyun /study/부록/searching/28.py
+++ KimYihyun /study/부록/searching/28.py
@@ -1,17 +1,20 @@
-# 고정점 찾기
+# 고정점 찾기 : 시간복잡도를 고려해 수정
+def binary_search(numbers):
+    left, right = 0, len(numbers) - 1
+    while left <= right:
+        mid = (left + right) // 2
+        if numbers[mid] == mid:
+            return mid
+        elif numbers[mid] < mid:
+            left = mid + 1
+        else:
+            right = mid - 1
+    return -1
+
+
 N = int(input())
 numbers = list(map(int, input().split()))
-fix = -1
 
-
-def binary_search(fix):
-    for i in range(len(numbers)):
-        if (i == numbers[i]):
-            fix = i
-            return fix
-    return fix
-
-
-res = binary_search(fix)
+res = binary_search(numbers)
 
 print(res)
