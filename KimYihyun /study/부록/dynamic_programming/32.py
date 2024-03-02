623aa392c7efb7764dc1044abfc9a1de422caff9
yhnny <93534188+yihyun-kim1@users.noreply.github.com>
2024-03-02 19:27:23 +0900

    Update 32.py with dynamic programming

--- KimYihyun /study/부록/dynamic_programming/32.py
+++ KimYihyun /study/부록/dynamic_programming/32.py
@@ -1,6 +1,7 @@
 # 정수 삼각형
 # https://www.acmicpc.net/problem/1932
 
+# DFS 코드 - 시간 초과
 N = int(input())  # 삼각형의 크기
 triangle = []
 
@@ -22,3 +23,33 @@ def dfs(row, col):
 
 
 print(dfs(0, 0))
+
+# dp 코드 - 통과
+# 정수 삼각형
+# https://www.acmicpc.net/problem/1932
+
+N = int(input())  # 삼각형의 크기
+triangle = []
+
+for i in range(N):
+    row = list(map(int, input().split()))
+    triangle.append(row)
+
+# 각 위치에서의 최대 합을 저장할 배열 초기화
+dp = [[0] * N for _ in range(N)]
+
+
+def dynamic_func():
+    # 맨 아래 줄부터 시작
+    for i in range(N - 1, -1, -1):
+        for j in range(i + 1):
+            if i == N - 1:
+                dp[i][j] = triangle[i][j]
+            else:
+                dp[i][j] = max(dp[i + 1][j], dp[i + 1][j + 1]) + triangle[i][j]
+
+    return dp[0][0]
+
+
+# 최대 합 계산 및 출력
+print(dynamic_func())
